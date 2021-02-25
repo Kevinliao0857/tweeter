918b32e4c27b3ec43d7d7f7edbd3c55f94006081
Kevinliao0857 <kevinliao0857@gmail.com>
2021-02-25 00:59:07 +0000

    loading tweets working?

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,74 +3,79 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
 $(document).ready(function() {
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
+loadtweets();
 
 
-const renderTweets = (tweets) => {
-for (let tweet of tweets) {
-  $("#tweets-container").append(createTweetElement(tweet))
-  }
-}
-
-const createTweetElement = (tweet) => {
-const $tweet = $("<article class='tweetPost'>")
+// Ajax POSTING
+$(".new-tweet form").on("submit", function(event) {
+  event.preventDefault();
 
 
-  const htmlTweetCode = `
+  $.ajax({
+    url: "/tweets",
+    data: $(this).serialize(),
+    method: "POST",
+    success:
+    function() {
+      loadtweets();
+      $textarea.val("");
+      $(".counter").text("140")
+    }
+  });
 
-    <header class="tweetHeader">
-      <img class="profileImage" src=${tweet.user.avatars}></img>
-      <div class="name">${tweet.user.name}</div>
-      <div class="username">${tweet.user.handle}</div>
-    </header>    
-      <article>
-        <p>${tweet.content.text}</p>
-      </article>
-    <footer class="footer">${tweet.created_at}
-    </footer>
-`;
-// const htmlTweetCode = (
-//          `<header class="tweetHeader">
-//           <img class="profileImage" src= "/images/profile-hex.png">
-//           <div class="name">person</div>
-//           <div class="username">@username</div>
-//         </header>       
-//           <article>
-//             <h3>Words</h3>
-//           </article>
-//           <footer class="footer">date
-//           </footer> `)
 
-return $tweet.html(htmlTweetCode)
-};
-
-renderTweets(data);
 });
 
 
+
+
+});
+
+
+
+/*render Tweets function*/
+const renderTweets = (tweets) => {
+  for (let tweet of tweets) {
+    $("#tweets-container").append(createTweetElement(tweet))
+    }
+  }
+  
+  
+  //Creating Tweet Element
+  const createTweetElement = (tweet) => {
+  const $tweet = $("<article class='tweetPost'>")
+  
+  
+    const htmlTweetCode = `
+  
+      <header class="tweetHeader">
+        <img class="profileImage" src=${tweet.user.avatars}></img>
+        <div class="name">${tweet.user.name}</div>
+        <div class="username">${tweet.user.handle}</div>
+      </header>    
+        <article>
+          <p>${tweet.content.text}</p>
+        </article>
+      <footer class="footer">${tweet.created_at}
+      </footer>
+  `;
+  
+  return $tweet.html(htmlTweetCode)
+  };
+  
+  
+  
+  //loadtweets function
+  const loadtweets = () => {
+    $.ajax("/tweets", {
+      method: "GET",
+      dataType: "JSON",
+      success: function(tweets) {
+        renderTweets(tweets)
+      } 
+  
+    })
+  };
